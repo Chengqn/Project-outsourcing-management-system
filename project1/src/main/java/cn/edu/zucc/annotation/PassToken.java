6b59c5fcf6c8fc072ccf8040f3bc6c3e6c826cc1
Chengqn <1347274664@qq.com>
2019-07-13 16:33:54 +0800

    重新上传

--- project1/src/main/java/cn/edu/zucc/annotation/PassToken.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.zucc.annotation;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target({ElementType.METHOD,ElementType.TYPE})
-public @interface PassToken {
-    boolean required() default  true;
-}
